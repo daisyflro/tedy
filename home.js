e109b4e123c4a01d3dd0d6ab335298bab310a6e2
Daisy Lopes <112718227+daisyflro@users.noreply.github.com>
2024-11-29 18:32:29 -0300

    Benefícios e planos adicionados.

--- home.js
+++ home.js
@@ -13,3 +13,22 @@ document.querySelectorAll('.faq-header').forEach(header => {
         faq.classList.toggle('active');
     });
 });
+
+$(function () {
+    $(document).on('click', '.number-spinner button', function () {
+        var btn = $(this),
+            oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+            newVal = 0;
+
+        if (btn.attr('data-dir') == 'up') {
+            newVal = parseInt(oldValue) + 1;
+        } else {
+            if (oldValue > 1) {
+                newVal = parseInt(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        btn.closest('.number-spinner').find('input').val(newVal);
+    });
+});
